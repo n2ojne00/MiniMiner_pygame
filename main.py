56d83520d64b6186b8f131b15d53f2ed8efbbf60
Netta <n2ojne00@students.oamk.fi>
2025-04-23 02:52:12 +0300

    movement limited to screen measurements

--- main.py
+++ main.py
@@ -9,27 +9,35 @@ SCREEN_HEIGHT = 700
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('GAMENAME')
 
-player = pygame.Rect((300, 250, 50, 50))
+player = pygame.Rect((400, 350, 50, 80))
 
 #start Game loop
 run = True
 while run == True:
     
+    #background color
     screen.fill((0, 20, 0))
     
     pygame.draw.rect(screen, (80, 130, 100), player)
     
-    #keybinds and cordinates
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0) 
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
     
+    #keybinds and coordinates
+    key = pygame.key.get_pressed()
+        #UP
+    if key[pygame.K_w] == True and player[1] >= 0: 
+        player.move_ip(0, -1)
+        #DOWN
+    elif key[pygame.K_s] == True and player[1] <= SCREEN_HEIGHT - player[3]:
+        player.move_ip(0, 1)    
+        #LEFT
+    elif key[pygame.K_a] == True and player[0] >= 0: 
+        player.move_ip(-1, 0) 
+        #RIGHT
+    elif key[pygame.K_d] == True and player[0] <= SCREEN_WIDTH - player[2]: 
+        player.move_ip(1, 0)
+    
+
+
     #stop Game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
